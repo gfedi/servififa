676bb9bd683d2a911a3007d4297cf1d4cfb73416
Giuseppe Broccolo <g.broccolo.7@gmail.com>
2018-09-15 10:35:43 +0100

    problema 2.a

---
+++ scripts/problema_2_a.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+import requests
+import pandas as pd
+from numpy import array
+
+
+PLAYERS_URL = ("https://raw.githubusercontent.com/mesosbrodleto/"
+              "soccerDataChallenge/master/players.json")
+EVENTS_URL = ("https://raw.githubusercontent.com/mesosbrodleto/"
+              "soccerDataChallenge/master/worldCup-final.json")
+ACCURATE_TAG = 1801
+
+
+def get_players():
+    """
+    get the list of players
+    """
+
+    return requests.get(PLAYERS_URL).json()
+
+
+def get_events():
+    """
+    get the list of events in the match
+    """
+
+    return requests.get(EVENTS_URL).json()
+
+
+def main():
+    match = get_events()
+    players = get_players()
+
+    events = pd.DataFrame(match,
+                          columns=['teamId', 'playerId', 'tags', 'eventId'])
+
+    passes_x_team = events.loc[events['eventId'] == 8].groupby(
+        ['teamId', 'playerId']).count()['tags'].to_dict()
+
+    accurate_passes_x_team = events.loc[(events['eventId'] == 8) & \
+        (events['tags'].apply(lambda x: {"id": ACCURATE_TAG} in x))].groupby(
+            ['teamId', 'playerId']).count()['tags'].to_dict()
+
+    results_d = list()
+    for t in events['teamId'].unique():
+        accuracy = array([accurate_passes_x_team[k] / float(passes_x_team[k])
+                          for k in accurate_passes_x_team if t in k])
+        results_d.append(
+            {'team': t, 'mean' : accuracy.mean(), 'std' : accuracy.std()})
+    results = pd.DataFrame(results_d).to_csv('problema_2_a.csv', index=False)
+
+
+if __name__ == "__main__":
+    main()
