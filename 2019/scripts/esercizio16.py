02ac4c8955469f0fb69b447118bf94fcec745345
Antonio <antoniotropiano@gmail.com>
2019-09-14 19:27:52 +0200

    esercizio 16 correzione

--- 2019/scripts/esercizio16.py
+++ 2019/scripts/esercizio16.py
@@ -13,21 +13,25 @@ list_players_in_match = {
     if x['matchId'] == 2576302 and x['playerId'] != 0}
 
 wrong_passes = []
-for player in [x for x in players_dict if x['wyId'] in list_players_in_match]:    
+for player in [x for x in players_dict if x['wyId'] in list_players_in_match]:
     n_wrong_passes = len([
         x for x in events_dict
         if 'pass' in x['subEventName'] and
-            1802 in [y['id'] for y in x['tags']] and
-                x['matchId'] == 2576302 and
-                    x['playerId'] == player['wyId']])
+        1802 in [y['id'] for y in x['tags']] and
+        x['matchId'] == 2576302 and
+        x['playerId'] == player['wyId']])
     wrong_passes.append(
         (str(player['wyId']),
          player['birthArea']['name'],
          str(n_wrong_passes)))
 
-wrong_passes.sort(key=lambda x: int(x[0]))
+
+wrong_passes_best = sorted(
+    wrong_passes, key=lambda x: int(x[2]), reverse=True)[0:3]
+
+wrong_passes_best.sort(key=lambda x: int(x[0]))
 
 with open("esercizio16.csv", "w") as f:
     f.write(";".join(csv_headers) + "\n")
-    for wrong_pass in wrong_passes:
+    for wrong_pass in wrong_passes_best:
         f.write(";".join(wrong_pass) + "\n")
