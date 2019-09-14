f3930cd6e0173667e1bbdaaf7319abb7141a9e4a
Giuseppe Broccolo <g.broccolo.7@gmail.com>
2019-09-14 13:59:58 +0100

    2019: risoluzione esercizio 20

---
+++ 2019/scripts/esercizio20.py
@@ -0,0 +1,24 @@
+import json as js
+
+csv_headers = ['playerId', 'n_parate']
+
+with open("events.json") as events:
+    events_dict = js.load(events)
+
+save_attempt = [x['playerId'] for x in events_dict 
+                if x['subEventName'] == 'Save attempt' and
+                    x['matchId'] == 2576302]
+n_save_attempt = [
+    (k, v) for k, v in {x:save_attempt.count(x) for x in save_attempt}.items()]
+
+n_save_attempt_multiplicity = [i[1] for i in n_save_attempt]
+if int(sum(n_save_attempt_multiplicity) / len(n_save_attempt_multiplicity)) == \
+        n_save_attempt_multiplicity[0]:
+    n_save_attempt.sort(key=lambda x: x[0])
+else:
+    n_save_attempt.sort(key=lambda x: x[1])
+
+with open("esercizio20.csv", "w") as f:
+    f.write(";".join(csv_headers) + "\n")
+    for x in n_save_attempt:
+        f.write("%s;%s\n" % (x[0], x[1]))
